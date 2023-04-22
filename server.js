17025f7360a4b60eed8e04b6ab774213714abbfe
Durvash <{ID}+{username}@users.noreply.github.com>
2023-04-22 15:23:14 +0530

    set config for mongoDB && have a make API for getting all product list

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const app = require('./app');
+const connectDatabase = require('./config/database');
 
 const dotenv = require('dotenv');
+
 //set config file
 dotenv.config({ path: 'config/config.env' });
+
+// connecting to Database
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
     console.log(`server started on PORT : ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
