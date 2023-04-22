03f18767838b84d1f02b447669ae3ed562a41408
Durvash <{ID}+{username}@users.noreply.github.com>
2023-04-22 17:23:57 +0530

    have an added product detail API

--- controllers/productController.js
+++ controllers/productController.js
@@ -7,3 +7,19 @@ exports.getProducts = async (req, res, next) => {
         products
     })
 }
+
+exports.getSingleProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    // console.log("data >> ",product);
+    if(!product) {
+        return res.status(404).json({
+            success: 0,
+            message: 'Product not found'
+        })
+    }
+
+    res.status(200).json({
+        success: 1,
+        product
+    })
+}
--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { getProducts } = require('../controllers/productController');
-
+const { getProducts, getSingleProduct } = require('../controllers/productController');
 
 router.route('/products').get(getProducts);
+router.route('/product/:id').get(getSingleProduct);
 
 module.exports = router;
