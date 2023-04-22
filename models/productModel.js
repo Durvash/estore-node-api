e737fb2cc1255e01f3419df4e54077717c78c86c
Durvash <{ID}+{username}@users.noreply.github.com>
2023-04-22 16:56:43 +0530

    have an added seeder file to add dummy data for products && also changed in product model regarding data type and validation messages

--- models/productModel.js
+++ models/productModel.js
@@ -1,65 +1,72 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Please enter product name'],
         trim: true,
-        maxLength: [100, 'Product name cannot exceed 100 characters']
-    },
-    price: {
-        type: Number,
-        required: [true, 'Please enter product price'],
-        maxLength: [5, 'Product name cannot exceed 5 characters'],
-        default: 0.0
+        maxLength: [100, 'Product name cannot exceed 60 characters']
     },
     description: {
         type: String,
         required: [true, 'Please enter product description'],
     },
-    ratings: {
+    price: {
+        type: Number,
+        required: [true, 'Please enter product price'],
+        default: 0
+    },
+    discountPercentage: {
         type: Number,
         default: 0
     },
-    images: [
-        {
-            public_id: {
-                type: String,
-                required: true
-            },
-            url: {
-                type: String,
-                required: true
-            },
-        }
-    ],
+    rating: {
+        type: Number,
+        default: 0
+    },
+    stock: {
+        type: Number,
+        required: [true, 'Please enter availibility stock for this product'],
+        default: 0
+    },
+    brand: {
+        type: String,
+        required: [true, 'Please enter product brand']
+    },
     category: {
         type: String,
         required: [true, 'Please select category for this product'],
         enum: {
             values: [
-                'Electronics',
-                'Cameras',
-                'Laptops',
-                'Accessories',
-                'Headphones',
-                'Food',
-                "Books",
-                'Clothes/Shoes',
-                'Beauty/Health',
-                'Sports',
-                'Outdoor',
-                'Home'
+                'smartphones',
+                'laptops',
+                'fragrances',
+                'skincare',
+                'groceries',
+                'home-decoration',
+                'furniture',
+                'tops',
+                'womens-dresses',
+                'womens-shoes',
+                'mens-shirts',
+                'mens-shoes',
+                'mens-watches',
+                'womens-watches',
+                'womens-bags',
+                'womens-jewellery',
+                'sunglasses',
+                'automotive',
+                'motorcycle',
+                'lighting'
             ],
             message: 'Please select correct category for product'
         }
     },
-    stock: {
-        type: Number,
-        required: [true, 'Please enter product stock'],
-        maxLength: [5, 'Product name cannot exceed 5 characters'],
-        default: 0
-    }
+    thumbnail: {
+        type: String,
+        required: [true, 'Please enter product thumbnail']
+    },
+    images: []
 })
 
 module.exports = mongoose.model('Product', productSchema);
