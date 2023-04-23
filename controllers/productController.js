cfb94a8934e728da68a9719d65ea3610887a58a4
Durvash <{ID}+{username}@users.noreply.github.com>
2023-04-23 06:17:47 +0530

    have an added filters on product list

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,16 @@
 const Product = require('../models/productModel');
 
 exports.getProducts = async (req, res, next) => {
-    const products = await Product.find();
+    // console.log(req.query);
+    var query = {}; // { category: req.query.category, price: { $gt: req.query.from_price, $lt: req.query.to_price} }
+    if(req.query.category) {
+        query = {...query, category: req.query.category};
+    }
+    if(req.query.from_price && req.query.to_price) {
+        query = {...query, price: { $gt: req.query.from_price, $lt: req.query.to_price}};
+    }
+    
+    const products = await Product.find(query);
     res.status(200).json({
         success: 1,
         products
@@ -9,17 +18,26 @@ exports.getProducts = async (req, res, next) => {
 }
 
 exports.getSingleProduct = async (req, res, next) => {
-    const product = await Product.findById(req.params.id);
-    // console.log("data >> ",product);
-    if(!product) {
-        return res.status(404).json({
+    try {
+        const product = await Product.findById(req.params.id);
+        // console.log("data >> ",product);
+        if(!product) {
+            return res.status(404).json({
+                success: 0,
+                message: 'Product not found'
+            })
+        }
+
+        res.status(200).json({
+            success: 1,
+            product
+        })
+        
+    } catch (error) {
+
+        res.status(404).json({
             success: 0,
-            message: 'Product not found'
+            message: `Product id is not valid`  // ${error.message}
         })
     }
-
-    res.status(200).json({
-        success: 1,
-        product
-    })
 }
