6230c8950c6910faf07a404923772d1de0d8b1b5
Durvash <{ID}+{username}@users.noreply.github.com>
2023-04-23 17:51:29 +0530

    have an added user account api && logout functionality, while user logout, it will not access account detail

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // import all routes
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser, loginUser } = require('../controllers/authController');
+const { registerUser, loginUser, userAccount, userLogout } = require('../controllers/authController');
 
 router.route('/signup').post(registerUser);
 router.route('/login').post(loginUser);
+router.route('/account').get(userAccount);
+router.route('/logout').get(userLogout);
 
 module.exports = router;
